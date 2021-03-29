31a9d7bce4e7fe246290bd17a7ebcf9a8f2080ca
Ilya Rozhkov <twentyny@yandex.ru>
2021-03-29 17:54:42 +0500

    initial commit, add main files

---
+++ test_main_page.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()

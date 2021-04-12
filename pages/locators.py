43e4ebc4e0c4b66e220b4a5736bc94a0dc52dc5f
Ilya Rozhkov <twentyny@yandex.ru>
2021-04-12 13:32:12 +0500

    add new check-methods

--- pages/locators.py
+++ pages/locators.py
@@ -9,7 +9,9 @@ class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
 
+
 class ProductPageLocators():
     ADD_TO_CART_BUTTON = (By.XPATH, '//form[@id="add_to_basket_form"]/child::button')
     BOOK_TITLE = (By.XPATH, '//div[@id="messages"]/descendant::strong')
     BOOK_TITLE_IN_MESSAGE = (By.XPATH, '//div[@class="col-sm-6 product_main"]/child::h1' )
+    SUCCESS_MESSAGE = (By.XPATH, '//strong[text()="Coders at Work"]' )
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,8 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class ProductPage(BasePage):
 
@@ -12,3 +14,11 @@ class ProductPage(BasePage):
         book_title = self.browser.find_element(*ProductPageLocators.BOOK_TITLE)
         book_title_in_message = self.browser.find_element(*ProductPageLocators.BOOK_TITLE_IN_MESSAGE)
         assert book_title.text == book_title_in_message.text, 'The fields have different names'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_dissapear_of_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"

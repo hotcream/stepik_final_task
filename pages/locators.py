a9d7c108353ddc49d970862a8f92dfa707ca18c8
Ilya Rozhkov <twentyny@yandex.ru>
2021-04-12 15:26:01 +0500

    add new locators and check-methods

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
@@ -14,4 +10,9 @@ class ProductPageLocators():
     ADD_TO_CART_BUTTON = (By.XPATH, '//form[@id="add_to_basket_form"]/child::button')
     BOOK_TITLE = (By.XPATH, '//div[@id="messages"]/descendant::strong')
     BOOK_TITLE_IN_MESSAGE = (By.XPATH, '//div[@class="col-sm-6 product_main"]/child::h1' )
-    SUCCESS_MESSAGE = (By.XPATH, '//strong[text()="Coders at Work"]' )
+    SUCCESS_MESSAGE = (By.XPATH, '//strong[text()="Coders at Work"]' ) # поменять!
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")

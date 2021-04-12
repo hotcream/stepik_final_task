4784b0af92628d4a370466f7870f41dd140299ef
Ilya Rozhkov <twentyny@yandex.ru>
2021-04-12 13:31:23 +0500

    add new tests

--- test_product_page.py
+++ test_product_page.py
@@ -13,9 +13,27 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()
     page.click_add_to_cart_button()
     page.solve_quiz_and_get_code()
     page.check_book_title()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
+    page = ProductPage(browser, 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207')
+    page.open()
+    page.click_add_to_cart_button()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser): 
+    page = ProductPage(browser, 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207')
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    page = ProductPage(browser, 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207')
+    page.open()
+    page.click_add_to_cart_button()
+    page.should_dissapear_of_success_message()
